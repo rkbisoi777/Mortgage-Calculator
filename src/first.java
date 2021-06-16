1966ff73dd8e5f775c23d644f39be00b71beee6d
rkbisoi777 <rkbisoi777@gmail.com>
2021-06-16 18:18:55 +0530

    OOPs First

---
+++ src/first.java
@@ -0,0 +1,11 @@
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class first
+{
+    public static void main(String[] args){
+        Mortgage mortgage1 = new Mortgage();
+        mortgage1.printMortgage(5000,1, (byte) 20);
+    }
+ }
